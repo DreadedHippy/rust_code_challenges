d965e95c4135a8ecfb705a299023976ddf3f9f9b
onotaizee@gmail.com <onotaizee@gmail.com>
2023-05-06 23:15:55 +0100

    fix: minor syntax error

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let mut number_of_test_cases: String = String::new();
     std::io::stdin()
-    .read_line(& mut number_of_test_cases)
+    .read_line(&mut number_of_test_cases)
     .unwrap();
 
     let number_of_test_cases: i32 = number_of_test_cases.trim().parse().unwrap();
