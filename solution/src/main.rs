132cbbfc9bc900bf6dbbc9958126775b82e4d906
onotaizee@gmail.com <onotaizee@gmail.com>
2023-05-06 23:09:05 +0100

    +added: boilerplate code

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let mut number_of_test_cases: String = String::new();
+    std::io::stdin()
+    .read_line(& mut number_of_test_cases)
+    .unwrap();
+
+    let number_of_test_cases: i32 = number_of_test_cases.trim().parse().unwrap();
+
+    for _test_case in 1..=number_of_test_cases {
+
+    }
+
 }
